23874aede85f634e6699873eb376a570de492d35
Ubuntu <ubuntu@ip-172-31-3-248.ap-southeast-2.compute.internal>
2013-08-06 14:23:07 +0000

    rough form

--- searchScreen.php
+++ searchScreen.php
@@ -1,3 +1,18 @@
-<?php
-	echo "Hello World!";
-?>
+<html>
+<head>
+<title>Search Wine Database</title>
+</head>
+<body>
+
+<form action="" method="">
+1. Wine Name:  <input type="text" name="wineName"><br>
+2. Winery Name: <input type="text" name="wineryName"><br>
+3. drop down list to be implemented <br>
+4. 
+5.
+6. Minimum number of wines in stock(per wine): <input type="text" name="minStock"><br>
+7. Minimum number of wines ordered(per wine): <input type="text" name="minOrdered"><br>
+</form>
+
+</body>
+</html>
