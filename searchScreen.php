cad546e8f359fe4f05ee19c28b0e4b4b796c755f
Ubuntu <ubuntu@ip-172-31-3-248.ap-southeast-2.compute.internal>
2013-08-08 12:43:25 +0000

    connect to server, query region, display region in dropdown list

--- searchScreen.php
+++ searchScreen.php
@@ -3,15 +3,42 @@
 <title>Search Wine Database</title>
 </head>
 <body>
-
+<?php
+   require_once('db.php');
+   if(!($connection = mysql_connect(DB_HOST, DB_USER, DB_PW)))
+   {
+      echo 'Could not connect to mysql on '. DB_HOST."\n";
+      exit;
+   }
+   if(!(mysql_select_db("winestore", $connection)))
+   {
+      echo 'Could not connect to database winestore\n';
+      exit;
+   }
+   $result = mysql_query("SELECT * FROM region", $connection);
+   if (!$result)
+   {
+      echo 'Could not run query: ' . mysql_error();
+      exit;
+   }
+?>
 <form action="" method="">
 1. Wine Name:  <input type="text" name="wineName"><br>
 2. Winery Name: <input type="text" name="wineryName"><br>
-3. drop down list to be implemented <br>
-4. 
-5.
+3. Region: <select value="tableName">
+<?php
+   while($row = mysql_fetch_row($result)) 
+   {
+      $region = "$row[0] ".$row[1];
+      echo '<option value="$row[0]">'.$region.'</option>';
+   }
+?>
+</select><br>
+4. <br>
+5. <br>
 6. Minimum number of wines in stock(per wine): <input type="text" name="minStock"><br>
 7. Minimum number of wines ordered(per wine): <input type="text" name="minOrdered"><br>
+<input type="submit" name="submit" value="search"><br>
 </form>
 
 </body>
