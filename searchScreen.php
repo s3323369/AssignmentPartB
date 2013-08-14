929e5e6fa378653c67a59a5c642bef98df6f766e
Ubuntu <ubuntu@ip-172-31-3-248.ap-southeast-2.compute.internal>
2013-08-14 11:12:29 +0000

    added drop down list year,grapevariety and table format

--- searchScreen.php
+++ searchScreen.php
@@ -21,25 +21,95 @@
       echo 'Could not run query: ' . mysql_error();
       exit;
    }
-?>
-<form action="" method="">
-1. Wine Name:  <input type="text" name="wineName"><br>
-2. Winery Name: <input type="text" name="wineryName"><br>
-3. Region: <select value="tableName">
-<?php
-   while($row = mysql_fetch_row($result)) 
+
+   $result2 = mysql_query("SELECT * FROM grape_variety", $connection);
+   if (!$result2)
    {
-      $region = "$row[0] ".$row[1];
-      echo '<option value="$row[0]">'.$region.'</option>';
+      echo 'Could not run query: ' . mysql_error();
+      exit;
+   }
+   $result3 = mysql_query("SELECT DISTINCT year FROM wine ORDER BY year ASC",
+ $connection);
+   if (!$result3)
+   {
+      echo 'Could not run query: ' . mysql_error();
+      exit;
+   }
+   $result4 = mysql_query("SELECT DISTINCT year FROM wine ORDER BY year DESC",
+ $connection);
+   if (!$result3)
+   {
+      echo 'Could not run query: ' . mysql_error();
+      exit;
    }
 ?>
-</select><br>
-4. <br>
-5. <br>
-6. Minimum number of wines in stock(per wine): <input type="text" name="minStock"><br>
-7. Minimum number of wines ordered(per wine): <input type="text" name="minOrdered"><br>
-<input type="submit" name="submit" value="search"><br>
-</form>
+<div>
+<table>
+<tr><td><strong>Winestore Database</strong></td></tr>
+<form action="" method="">
+<tr>
+   <td>1. Wine Name: </td>
+   <td> <input type="text" name="wineName"></td>
+</tr>
+<tr>
+   <td>2. Winery Name:</td>
+   <td> <input type="text" name="wineryName"></td>
+<tr>
+   <td>3. Region:</td>
+   <td> <select value="regionTable">
+<?php
+   while($row1 = mysql_fetch_row($result))
+   {
+      $region = "$row1[0] ".$row1[1];
+      echo '<option value="$row1[0]">'.$region.'</option>';
+   }
+?>
+   </select></td>
+</tr>
 
+<tr>
+   <td>4. Grape Variety: </td>
+   <td> <select value="grapeTable">
+<?php
+   while($row2 = mysql_fetch_row($result2))
+   {
+      $grape_variety = "$row2[0] ".$row2[1];
+      echo '<option value="$row2[0]">'.$grape_variety.'</option>';
+   }
+?>
+   </td></select>
+<tr>
+   <td>5. Year from: <select value="minYear">
+<?php
+   while($row3 = mysql_fetch_row($result3))
+   {
+      $lowerYear = $row3[0];
+      echo '<option value="lowerBound">'.$lowerYear.'</option>';
+   }
+?>
+   </td>
+   <td>to: <select value="maxYear">
+<?php
+   while($row4 = mysql_fetch_row($result4))
+   {
+      $upperYear = $row4[0];
+      echo '<option value="upperBound">'.$upperYear.'</option>';
+   }
+?>
+   </select></td>
+</tr>
+<tr>
+   <td>6. Minimum number of wines in stock(per wine):</td>
+   <td> <input type="text" name="minStock"></td>
+<tr>
+   <td>7. Minimum number of wines ordered(per wine):</td>
+   <td> <input type="text" name="minOrdered"></td>
+</tr>
+<tr>
+   <td><input type="submit" name="submit" value="search"></td>
+</tr>
+</form>
+</table>
+</div>
 </body>
 </html>
