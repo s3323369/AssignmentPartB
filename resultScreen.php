233bd9ab5197e36db91c477fe5025fbf0063a652
Ubuntu <ubuntu@ip-172-31-3-248.ap-southeast-2.compute.internal>
2013-08-14 11:25:54 +0000

    add resultScreen.php

---
+++ resultScreen.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Winestore search results</title>
+</head>
+<body>
+<table>
+<tr>
+<td>Name </td>
+<td>Grape Variety </td>
+<td>Year </td>
+<td>Winery </td>
+<td>Region </td>
+<td>Cost of the wine in the inventory </td>
+<td>Total number of bottles available at any price </td>
+<td>Total stock sold of the wine </td>
+<td>Total sales revenue for the wine </td>
+</tr>
+</table>
+</body>
+</html>
