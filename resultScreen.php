f104e078e9b1bceb9102b88eb627a1ad195ddc55
Ubuntu <ubuntu@ip-172-31-3-248.ap-southeast-2.compute.internal>
2013-08-27 15:49:19 +0000

    display sales revenue, no results found message, implemented cost range search

--- resultScreen.php
+++ resultScreen.php
@@ -22,15 +22,19 @@
    $maxYear = $_GET['maxYearTable'];
    $minStock = $_GET['minStock'];
    $minOrdered = $_GET['minOrdered'];
-   $query = "SELECT wine_name, variety, year, winery_name, region_name,
-	     on_hand, cost
+   $minCost = $_GET['minCost'];
+   $maxCost = $_GET['maxCost'];
+   $query = "SELECT DISTINCT wine.wine_id, wine_name, variety, year,
+	     winery_name, region_name, on_hand, cost, SUM(qty) AS orders,
+	     SUM(price) AS totalprice
                   FROM wine, winery, grape_variety, wine_variety, region,
-		  inventory
+		  inventory, items
                   WHERE wine.winery_id = winery.winery_id
 		  AND region.region_id = winery.region_id
                   AND wine.wine_id = wine_variety.wine_id
                   AND wine_variety.variety_id = grape_variety.variety_id
-		  AND inventory.wine_id = wine.wine_id";
+		  AND inventory.wine_id = wine.wine_id
+		  AND wine.wine_id = items.wine_id";
 
    if($wineName != "")
    {
@@ -44,7 +48,7 @@
    {
       $query .= " AND region_name = '{$regionName}'";
    }
-   if($grapeVariety != "")
+   if($grapeVariety != "" && $grapeVariety != "All")
    {
       $query .= " AND variety = '{$grapeVariety}'";
    }
@@ -53,9 +57,39 @@
       $query .= " AND year <= {$maxYear}";
       $query .= " AND year >= {$minYear}";
    }
+   if($minCost != "")
+   {
+      if($maxCost != "")
+      {
+         if($minCost <= $maxCost)
+         {
+            $query .= " AND cost <= {$maxCost}";
+            $query .= " AND cost >= {$minCost}";
+         }
+         else
+         {
+            echo "min cost should be lower than max cost\n";
+            exit;
+         }
+      }
+      else
+      {
+         $query .= " AND cost >= {$minCost}";
+      }
+   }
+   else if($maxCost != "")
+   {
+      $query .= " AND cost <= {$maxCost}";
+   }
    if($minStock != "")
    {
       $query .= " AND on_hand >= {$minStock}";
+   }
+
+   $query .= " GROUP BY items.wine_id";
+   if($minOrdered != "")
+   {
+      $query .= " HAVING orders >= {$minOrdered}";
    }
    $query .= " ORDER BY wine_name ASC";
    $result = mysql_query("$query", $connection);
@@ -66,6 +100,10 @@
    }   
 ?>
 <body>
+<?php
+   if(mysql_num_rows($result))
+   {
+?>
 <table>
 <tr>
 <td>Name </td>
@@ -88,7 +126,13 @@ while($row = @mysql_fetch_array($result))
    ."\n<td>{$row["region_name"]}</td>"
    ."\n<td>{$row["cost"]}</td>"
    ."\n<td>{$row["on_hand"]}</td>"
-   ."\n<td>{$row["SUM(qty)"]}</td>\n</tr>";
+   ."\n<td>{$row["orders"]}</td>"
+   ."\n<td>{$row["totalprice"]}</td>\n</tr>";
+}
+}
+else
+{
+   echo "No records match the search criteria";
 }
 ?>
 </table>
